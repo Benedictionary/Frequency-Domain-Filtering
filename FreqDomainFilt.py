afa51a4d3b245868c9d7bd5eacdb4ae5d1532456
Benedictionary <summitless@gmail.com>
2019-05-07 18:12:21 -0700

    Gaussian Code Works

--- FreqDomainFilt.py
+++ FreqDomainFilt.py
@@ -3,8 +3,8 @@ import matplotlib.pyplot as mplplt
 import matplotlib.image as mplimg
 
 ##Upload Images to Test
-#img = mplimg.imread(r'testImage.jpg')
-img = mplimg.imread(r'Monarch.jpg')
+img = mplimg.imread(r'testImage.jpg')
+#img = mplimg.imread(r'Monarch.jpg')
 
 ##Convert Image Into Grayscale
 def rgb2gray(image):
@@ -19,13 +19,10 @@ mplplt.title("Original Image in Grayscale")
 mplplt.show()
 
 a = [[1,0,0,1,2,3,4,5,6,7,8,9,0,0,0],[2,0,0,1,2,3,4,5,6,7,8,9,0,0,0], [3,0,0,1,2,3,4,5,6,7,8,9,0,0,0]]
-imgXSize = 16
+print("Dimensions: ",numpy.shape(a))
+imgXSize = 15
 imgYSize = 3
-aNew = numpy.delete(a, range((imgXSize-3-1),imgXSize), 1)
-#aNew = numpy.delete(aNew, range((imgYSize-1),imgYSize), 0)
-#aNew = numpy.delete(aNew, range(0,1), 0)
-#aNew = numpy.delete(aNew, range(0,3), 1)
-print(aNew)
+#print(aNew)
 
 
 
@@ -37,7 +34,6 @@ print(aNew)
 
 #Find the Size of Each Dimension of the Original Image
 imgYSize, imgXSize = numpy.shape(grayImg)
-
 
 def padImage(img, imgXSize, imgYSize):
     padOneSideX = numpy.int(imgXSize/2) #numpy.int finds integer floor of argument
@@ -65,6 +61,7 @@ centeredImg = centerImage(paddedImg,paddedXSize,paddedYSize)
 print(centeredImg)
 print(centeredImg[1,2])
 
+#Compute the DFT
 def DFT(centeredimg):
     DFTImage = numpy.fft.fft2(centeredImg)
     return DFTImage
@@ -72,18 +69,54 @@ def DFT(centeredimg):
 DFTImg = DFT(centeredImg)
 magDFTImg = numpy.abs(DFTImg)
 magDFTImg = 20*numpy.log(magDFTImg)
-magDFTImg = magDFTImg/numpy.amax(magDFTImg)*255 #Normalize the magnitude spectrum
 magDFTImg = magDFTImg.astype(int)
 
 mplplt.imshow(magDFTImg, cmap = 'gray')
 mplplt.title("DFT of Image")
 mplplt.show()
 
+DFTYSize, DFTXSize = numpy.shape(paddedImg)
+#Function for Creating any MxN Size Gaussian Kernal based on Gaussian Formula
+def createGaussian(XSize, YSize, sigma): #Accepts Side Lengths Only
+    gaussian = numpy.zeros((YSize, XSize))
+    sideXMax = numpy.int(numpy.floor(XSize / 2))
+    sideXMin = -sideXMax
+    sideYMax = numpy.int(numpy.floor(YSize / 2))
+    sideYMin = -sideYMax
+    if sideXMax % 2==0:
+        sideXMax -= 1
+    if sideYMax % 2 == 0:
+        sideYMax -= 1
+    total = 0
+    for s in range(sideYMin, sideYMax+1):
+        for t in range(sideXMin, sideXMax + 1):
+            gaussian[s+sideYMax][t+sideXMax] = numpy.exp(-(s*s+t*t)/(2*sigma*sigma))
+            total += gaussian[s+sideYMax][t+sideXMax]
+    return gaussian
+
+gauss = createGaussian(DFTXSize,DFTYSize,100)
+
+print("SizeDFT:", numpy.shape(DFTImg))
+print("SizeGauss:", numpy.shape(gauss))
+gaussianFiltDFT = gauss*DFTImg
+
+magDFTImg = numpy.abs(gaussianFiltDFT)
+magDFTImg = 20*numpy.log(magDFTImg)
+print("magDFTMax: ",numpy.amax(magDFTImg))
+magDFTImg = magDFTImg.astype(int)
+
+'''
+mplplt.imshow(magDFTImg, cmap = 'gray')
+mplplt.title("Gaussian'd DFT of Image")
+mplplt.show()
+'''
+
+#Compute the iDFT
 def iDFT(fft):
     filteredImg = numpy.fft.ifft2(fft)
     return filteredImg
 
-filteredImg = iDFT(DFTImg)
+filteredImg = iDFT(gaussianFiltDFT)
 filteredImg = numpy.abs(filteredImg)
 filteredImg = filteredImg.astype(int)
 print(filteredImg)
@@ -95,8 +128,8 @@ mplplt.show()
 def unpad(img, imgXSize, imgYSize):
     padOneSideX = numpy.int(imgXSize/2) #numpy.int finds integer floor of argument
     padOneSideY = numpy.int(imgYSize/2)
-    imgUnpad = numpy.delete(img, range((imgXSize*2 - padOneSideX - 1), imgXSize*2), 1)
-    imgUnpad = numpy.delete(imgUnpad, range((imgYSize*2 - padOneSideY-1), imgYSize*2), 0)
+    imgUnpad = numpy.delete(img, range((imgXSize*2 - padOneSideX), imgXSize*2), 1)
+    imgUnpad = numpy.delete(imgUnpad, range((imgYSize*2 - padOneSideY), imgYSize*2), 0)
     imgUnpad = numpy.delete(imgUnpad, range(0, padOneSideY), 0)
     imgUnpad = numpy.delete(imgUnpad, range(0, padOneSideX), 1)
     return imgUnpad
@@ -106,21 +139,6 @@ mplplt.imshow(unpaddedFilteredImg, cmap = 'gray')
 mplplt.title("Unpadded Filtered Image")
 mplplt.show()
 
-
-'''
-#Function for Creating any MxN Size Gaussian Kernal based on Gaussian Formula
-def createGaussian(XSize, YSize, sigma):
-    gaussian = numpy.zeros((len, len))
-    sideXMax = numpy.int(numpy.floor(len / 2))
-    sideXMin = -sideMax
-    total = 0
-    for s in range(sideMin, sideMax+1):
-        for t in range(sideMin, sideMax + 1):
-            gaussian[s+sideMax][t+sideMax] = numpy.exp(-(s*s+t*t)/(2*sigma*sigma))
-            total += gaussian[s+sideMax][t+sideMax]
-    gaussian = gaussian/total
-    return gaussian
-'''
 
 
 print("Hello World")
